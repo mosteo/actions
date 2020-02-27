7d5f20f7e1fc8f8162d54dee79d129521869aa18
Alejandro R. Mosteo <amosteo@unizar.es>
2020-02-27 18:47:03 +0100

    Pull image

--- docker-run/main.js
+++ docker-run/main.js
@@ -1,8 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { dockerCommand } = require('docker-cli-js');
 
 try {
    console.log(`Hello Actionworld`);
+
+   // docker options
+   const options = {
+      machineName:             null, // local docker
+      currentWorkingDirectory: null, // current working directory
+      echo:                    true, // echo command output
+   };
+ 
+   // pull the required machine
+   const data = await dockerCommand
+     (`pull ${core.getInput('docker-image')}`, options);
+
 } catch (error) {
    core.setFailed(error.message);
 }
