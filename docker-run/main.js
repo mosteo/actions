a56c1d0d80a79c67e7299fda7577846ddf518145
Alejandro R. Mosteo <amosteo@unizar.es>
2020-02-28 23:11:11 +0100

    More params

--- docker-run/main.js
+++ docker-run/main.js
@@ -11,11 +11,20 @@ try {
       currentWorkingDirectory: null, // current working directory
       echo:                    true, // echo command output
    };
+
+   const dockerImage = core.getInput('docker-image');
+   const hostDir     = core.getInput('host-dir');
+   const guestDir    = core.getInput('guest-dir');
+   const command     = core.getInput('command');
  
    // pull the required machine
-   dockerCommand (`pull ${core.getInput('docker-image')}`, options)
-     .then(data => { console.log ('Pulled OK ' + data); })
-     .catch(err => { console.log ('Pulled Err ' + err); });
+   dockerCommand (`pull ${dockerImage}`, options)
+     .then(() => console.log (`Pulled OK: ${dockerImage}`))
+     .catch(err => console.log (`Pulled Err: ${dockerImage} ${err}`))
+   // run it
+     .then(() => dockerCommand (`run -w ${guestDir} -v${hostDir}:${guestDir} ${dockerImage} ${command}`))
+     .then(() => console.log (`Ran OK: ${command}`))
+     .catch(err => console.log (`Run Err: ${dockerImage} ${command}: error is: ${err}`));
 
 } catch (error) {
    core.setFailed(error.message);
