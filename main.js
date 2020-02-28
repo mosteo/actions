0f5c38b1cb71ce978d7af08bc5d56386ad772b62
Alejandro R. Mosteo <amosteo@unizar.es>
2020-02-29 00:44:51 +0100

    Move to core.info

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const github = require('@actions/github');
 const { dockerCommand } = require('docker-cli-js');
 
 try {
-   console.log(`Hello Actionworld`);
-
    // docker options
    const options = {
       machineName:             null, // local docker
@@ -16,14 +14,17 @@ try {
    const hostDir  = core.getInput('host-dir');
    const guestDir = core.getInput('guest-dir');
    const command  = core.getInput('command');
+
+   core.info(`PATH is ${process.env.PATH}`);
  
    // pull the required machine
    dockerCommand (`pull ${image}`, options)
-     .then(() => console.log (`Pulled OK: ${image}`))
+     .then(() => core.info (`Pulled OK: ${image}`))
      .catch(err => core.setFailed (`Pulled Err: ${image} ${err}`))
+
    // run it
      .then(() => dockerCommand (`run -e PATH -w ${guestDir} -v${hostDir}:${guestDir} ${image} ${command}`))
-     .then(() => console.log (`Ran OK: ${command}`))
+     .then(() => core.info (`Ran OK: ${command}`))
      .catch(err => core.setFailed (`Run Err: ${image} ${command}: error is: ${err}`));
 
 } catch (error) {
