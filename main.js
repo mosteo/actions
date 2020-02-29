371ccc21d41807061e496f65de868c0129b1d019
Alejandro R. Mosteo <amosteo@unizar.es>
2020-02-29 14:10:40 +0100

    Use await syntax and default options

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { dockerCommand } = require('docker-cli-js');
-
-try {
-   // docker options
-   const options = {
-      machineName:             null, // local docker
-      currentWorkingDirectory: null, // current working directory
-      echo:                    true, // echo command output
-   };
-
-   const image    = core.getInput('image');
-   const hostDir  = core.getInput('host-dir');
-   const guestDir = core.getInput('guest-dir');
-   const command  = core.getInput('command');
-   const params   = core.getInput('params');
-
-   core.info(`PATH is ${process.env.PATH}`);
  
-   // pull the required machine
-   dockerCommand (`pull ${image}`, options)
-     .then(() => core.info (`Pulled OK: ${image}`))
-     .catch(err => core.setFailed (`Pulled Err: ${image} ${err}`))
+async function run() {
+   try {
+      const image    = core.getInput('image');
+      const hostDir  = core.getInput('host-dir');
+      const guestDir = core.getInput('guest-dir');
+      const command  = core.getInput('command');
+      const params   = core.getInput('params');
 
-   // run it
-     .then(() => dockerCommand (`run ${params} -w ${guestDir} -v${hostDir}:${guestDir} ${image} ${command}`))
-     .then(() => core.info (`Ran OK: ${command}`))
-     .catch(err => core.setFailed (`Run Err: ${image} ${command}: error is: ${err}`));
+      core.info(`Host PATH: ${process.env.PATH}`);
+    
+      // pull the required machine
+      await dockerCommand(`pull ${image}`);
+      core.info(`Pulled OK: ${image}`);
 
-} catch (error) {
-   core.setFailed(error.message);
-}
+      // run it
+      await dockerCommand(`run ${params} -w ${guestDir} -v${hostDir}:${guestDir} ${image} ${command}`);
+      core.info (`Ran OK: ${command}`);
+
+   } catch (error) {
+      core.setFailed(error.message);
+   }
+};
+
+run();
